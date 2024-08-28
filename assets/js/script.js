378c84cc0b9016ad84f60ba0a87a2151d7fcefaf
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-08-28 17:10:22 +0000

    Remove redundant code

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,22 +33,10 @@ document.getElementById('answer-clue').addEventListener('change', function(event
     }
 });
 
-    let radios = document.querySelectorAll("input[type=radio]");
-
-    for (let btn of radios) {
-        btn.addEventListener("click", function() {
-        
-        let valueKey = this.getAttribute('value');
-        let gameType = mySafariQuestions[mylist[0]]['Species']
-            
-        console.log('The game type is',gameType);
-        console.log(`Users choice is',${mySafariQuestions[mylist[0]][valueKey]}`);
-        userchoice=mySafariQuestions[mylist[0]][valueKey];
-        
-        myCheckAnswer(gameType,userchoice)
+    
     })
-}}
-)
+
+
 
 // The gameType and valueKey will be pushed inside this list
 let mylist = []
