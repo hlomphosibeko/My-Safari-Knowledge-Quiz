5e12994c7ec093cb9a4235dffd712c0ed5cd7c32
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-08-28 06:31:36 +0000

    Add for loop to the event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     
     let radios = document.querySelectorAll("input[type=radio]");
+
+    for (let btn of radios) {
+        btn.addEventListener("click", function() {
+            let valueKey = this.getAttribute('value');
