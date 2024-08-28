f648e444c03e8a89b720e25d33e5f4cab7335fd5
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-08-28 06:37:11 +0000

    Add the buttons that have to be listened to by the event listener, console the actions to see if the code is indeed funtioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let btn of radios) {
         btn.addEventListener("click", function() {
-            let valueKey = this.getAttribute('value');
+        
+        let valueKey = this.getAttribute('value');
+        let gameType=mySafariQuestions[mylist[0]]['Species']
+            
+        console.log('The game type is',gameType);
+        console.log(`Users choice is',${mySafariQuestions[mylist[0]][valueKey]}`);
+        userchoice=mySafariQuestions[mylist[0]][valueKey];
